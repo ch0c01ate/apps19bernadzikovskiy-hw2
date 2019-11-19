3c5aa037a1280e17f979b34f443fa71a352fc597
Bernadzikovskiy Slavik <vbernadzikovskiy@gmail.com>
2019-11-19 17:15:07 +0200

    Made immutable classes final

--- src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
+++ src/main/java/ua/edu/ucu/collections/immutable/ImmutableArrayList.java
@@ -1,6 +1,6 @@
 package ua.edu.ucu.collections.immutable;
 
-public class ImmutableArrayList implements ImmutableList {
+public final class ImmutableArrayList implements ImmutableList {
     private Object[] array;
     private static int NOTFOUNDSTATUS = -1;
 
